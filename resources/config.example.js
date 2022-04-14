c53154335b7344ef6f3f347d5da794ec67b95754
ezxmora <erik1997@hotmail.es>
2022-04-15 00:47:27 +0100

    Config changes

--- resources/config.example.js
+++ resources/config.example.js
@@ -4,19 +4,19 @@ module.exports = {
   // Only works with debug enabled
   prefix: '!',
   ownerID: 'BOT OWNER ID',
-  token: process.env.FRISKAINET_TOKEN || 'DISCORD TOKEN',
+  token: process.env.FRISKAINET_TOKEN ?? 'DISCORD TOKEN',
   channels: {
     volatile: 'VOLATILE CHANNEL NAME',
     pinneds: 'PINS CHANNEL NAME',
     logs: 'LOGS CHANNEL NAME',
   },
-  adminRole: process.env.FRISKAINET_ADMIN_ROLE,
-  betRatio: process.env.FRISKAINET_BETTING_RATIO,
-  databaseURL: process.env.FRISKAINET_DATABASE_URL || 'DATABASE URL',
-  voicerssToken: process.env.FRISKAINET_VOICERSS || 'VOICE RSS TOKEN',
+  adminRole: 'ADMIN ROLE NAME',
+  betRatio: 'BETTING SYSTEM PROFIT RATIO',
+  databaseURL: process.env.FRISKAINET_DATABASE_URL ?? 'DATABASE URL',
+  voicerssToken: process.env.FRISKAINET_VOICERSS ?? 'VOICE RSS TOKEN',
   spotify: {
-    clientId: process.env.FRISKAINET_SPOTIFY_CLIENTID || 'SPOTIFY CLIENT ID',
-    clientSecret: process.env.FRISKAINET_SPOTIFY_CLIENTSECRET || 'SPOTIFY CLIENT SECRET',
+    clientId: process.env.FRISKAINET_SPOTIFY_CLIENTID ?? 'SPOTIFY CLIENT ID',
+    clientSecret: process.env.FRISKAINET_SPOTIFY_CLIENTSECRET ?? 'SPOTIFY CLIENT SECRET',
   },
   presence: {
     name: 'Anything you want here',
@@ -25,7 +25,9 @@ module.exports = {
     // Check https://discord.js.org/#/docs/main/stable/typedef/ClientPresenceStatus
     status: 'online',
   },
-  randomizerRoute: process.env.FRISKAINET_RANDOMIZER_ROUTE || 'POKEMON RANDOMIZER ROUTE',
+  randomizerRoute: process.env.FRISKAINET_RANDOMIZER_ROUTE ?? 'POKEMON RANDOMIZER ROUTE',
+  challongeUsername: 'YOUR CHALLONGE USERNAME',
+  challongeApiKey: process.env.FRISKAINET_CHALLONGE_API_KEY ?? 'YOUR CHALLONGE API KEY',
   commands: {
     economy: {
       daily: true,
