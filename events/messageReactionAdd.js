f4b65a8e276a3d194911da658cdf461f128b829c
ezxmora <erik1997@hotmail.es>
2022-05-13 12:30:47 +0100

    Added role assigner

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -2,6 +2,23 @@ module.exports = {
   name: 'messageReactionAdd',
   once: false,
   execute: async (reaction, user, bot) => {
+    // Message or reaction might not be cached, so we fetch it
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch();
+    // Ignore bots
+    if (user.bot) return;
+    // Ignore reactions outside guilds
+    if (!reaction.message.guild) return;
+
+    const {
+      config: {
+        roleAssignerMessageId,
+        channels: { pinneds },
+        roles: { random, vip },
+      },
+    } = bot;
+
+    // Message pinner
     if (reaction.emoji.name === '📌') {
       const pin = bot.database.Pin;
       const isPinned = await pin.findOne({
@@ -13,21 +30,10 @@ module.exports = {
       // We ommit messages that are already pinned
       if (isPinned?.pinned) return;
 
-      // Message might not be cached, so we fetch it
-      if (reaction.partial) {
-        try {
-          await reaction.fetch();
-        }
-        catch (err) {
-          console.log(`Ha habido un error al cachear el mensaje, ${err}`);
-          return;
-        }
-      }
-
       const serverOwner = await reaction.message.guild.fetchOwner();
 
       if (reaction.count >= 3 || serverOwner.user.id === user.id) {
-        const channel = await reaction.message.guild.channels.cache.find((c) => c.name === bot.config.channels.pinneds);
+        const channel = await reaction.message.guild.channels.cache.find((c) => c.name === pinneds);
 
         const embedPin = {
           color: '#FF0000',
@@ -35,7 +41,7 @@ module.exports = {
             name: reaction.message.author.tag,
             icon_url: reaction.message.author.avatarURL({ dynamic: true, format: 'png' }),
           },
-          description: `${reaction.message.content} [...ir al mensaje](${reaction.message.url})`,
+          description: `${reaction.message.content}\n\n[...ir al mensaje](${reaction.message.url})`,
           footer: { text: '📌' },
           timestamp: new Date(reaction.message.createdTimestamp),
         };
@@ -50,6 +56,21 @@ module.exports = {
 
         const pinnedMessage = await channel.send({ embeds: [embedPin] });
         pin.create({ pinId: pinnedMessage.id, messageId: reaction.message.id, pinned: true });
+      }
+    }
+
+    // Role assigner
+    if (reaction.message.id === roleAssignerMessageId) {
+      const [randomRole, vipRole] = reaction.message.guild.roles.cache.filter((r) => r.name === random || r.name === vip);
+
+      if (reaction.emoji.name === '🔵') {
+        await reaction.message.guild.members.cache.get(user.id).roles.add(randomRole);
+        await reaction.users.remove(user);
+      }
+
+      if (reaction.emoji.name === '🟢') {
+        await reaction.message.guild.members.cache.get(user.id).roles.add(vipRole);
+        await reaction.users.remove(user);
       }
     }
   },
